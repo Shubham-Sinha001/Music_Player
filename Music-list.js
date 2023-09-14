bf78136ded424bee11e93a324497f34bc33cd629
Shubham-Sinha001 <144666404+Shubham-Sinha001@users.noreply.github.com>
2023-09-14 11:41:49 +0530

    Add files via upload

---
+++ Music-list.js
@@ -0,0 +1,41 @@
+// here i creat an object that contains all music name and details
+
+let  allMusic = 
+[
+    {
+        name: "Amplifier - Imran Khan",
+        artist: "Official Music Studio",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Bewafa - Imran Khan",
+        artist: "Official Music Studio",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Billo - J Star",
+        artist: "J STAR Productions",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "J STAR|HULAR",
+        artist: "J STAR Productions",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "NA NA NA|J STAR",
+        artist: "J STAR Productions",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "STISFAY-| I am a Rider|Imran Khan",
+        artist: "(Official Music Studio) ",
+        img: "music-6",
+        src: "music-6"
+    }
+]
